e22cc114761f100549898fee9753a066184541dd
Moritz Bruder <muesli4@gmail.com>
2016-04-29 15:35:16 +0200

    Improved test binary to use tables on top of tables.

--- src/Test.hs
+++ src/Test.hs
@@ -5,17 +5,23 @@ import Control.Monad
 import Text.Layout.Table
 
 main :: IO ()
-main = 
-    forM_ styles $ \style ->
-        forM_ layouts $ \layout -> (putStrLn "" >>) $ (print layout >>) $ mapM_ putStrLn $
-            layoutTableToLines [ rowGroup [ [longText, smallNum]
-                                          , [shortText, bigNum]
-                                          ]
-                               ]
-                               (Just (["Some text", "Some numbers"], repeat centerHL))
-                               [layout, layout]
-                               style
+main = putStrLn $ layoutTableToString rowGroups
+                                      (Just (["Layout", "Result"], repeat centerHL))
+                                      [fixedLeftL 20, LayoutSpec Expand CenterPos NoAlign noCutMark]
+                                      unicodeRoundS
   where
+    rowGroups    = flip concatMap styles $ \style ->
+        flip map layouts $ \layout ->
+            rowGroup $ columnsAsGrid [ justifyText 20 $ show layout
+                                     , genTable layout style
+                                     ]
+    genTable l s = layoutTableToLines [ rowGroup [ [longText, smallNum]
+                                                 , [shortText, bigNum]
+                                                 ]
+                                      ]
+                                      (Just (["Some text", "Some numbers"], repeat centerHL))
+                                      [l, l]
+                                      s
     longText  = "This is long text"
     shortText = "Short"
     bigNum    = "200300400500600.2"
