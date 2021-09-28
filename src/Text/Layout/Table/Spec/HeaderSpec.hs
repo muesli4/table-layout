316e7329724c33b73588ce68a009ce8553f82802
Stephen Morgan <morgan.stephen99@gmail.com>
2021-09-28 22:00:47 +1000

    Add functions and instances for HeaderSpec

--- src/Text/Layout/Table/Spec/HeaderSpec.hs
+++ src/Text/Layout/Table/Spec/HeaderSpec.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Text.Layout.Table.Spec.HeaderSpec where
 
+import Data.Bifunctor
 import Data.Default.Class
+import Data.List
 
 import Text.Layout.Table.Spec.HeaderColSpec
 
@@ -14,6 +18,11 @@ import Text.Layout.Table.Spec.HeaderColSpec
 data HeaderSpec sep a
     = HeaderHS sep [HeaderColSpec] [a]
     | NoneHS sep
+  deriving (Functor)
+
+instance Bifunctor HeaderSpec where
+    bimap f g (HeaderHS sep specs titles) = HeaderHS (f sep) specs (map g titles)
+    bimap f _ (NoneHS sep)                = NoneHS (f sep)
 
 -- | By the default the header is not shown.
 instance Default sep => Default (HeaderSpec sep a) where
@@ -38,3 +47,13 @@ fullH = fullSepH ()
 -- | Use titles with the default header column specification and separator.
 titlesH :: [a] -> HeaderSpec () a
 titlesH = fullSepH () (repeat def)
+
+-- | Zip a 'HeaderSpec' with a list.
+zipHeader :: [b] -> HeaderSpec sep a -> HeaderSpec sep (b, a)
+zipHeader _  (NoneHS sep)                = NoneHS sep
+zipHeader xs (HeaderHS sep specs titles) = HeaderHS sep specs (zip xs titles)
+
+-- | Flatten a header to produce a list of content and separators.
+flattenHeader :: HeaderSpec sep a -> [Either sep a]
+flattenHeader (NoneHS _)              = []
+flattenHeader (HeaderHS sep _ titles) = intersperse (Left sep) $ map Right titles
